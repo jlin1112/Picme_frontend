de5394b559b3349dbd58c235c40afd01529b3659
jlin1112 <105515232+jlin1112@users.noreply.github.com>
2024-03-12 17:01:36 -0700

    axios update

--- src/pages/Picmes.tsx
+++ src/pages/Picmes.tsx
@@ -128,7 +128,7 @@ const Picmes: React.FC = () => {
   useEffect(() => {
     
     setIsLoading(true);
-    Axios.post(`${apiUrl}/picmes`, { page: currentPage })
+    Axios.post(`picme-backend.vercel.app/picmes`, { page: currentPage })
       .then((response) => {
         console.log('data')
         setData(response.data.picmes);
