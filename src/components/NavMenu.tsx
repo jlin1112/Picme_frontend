2721d1f968f9899747f13083a96fa20ca03303ba
jlin1112 <105515232+jlin1112@users.noreply.github.com>
2024-03-15 19:47:10 -0700

    Update NavMenu.tsx

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -30,7 +30,6 @@ export default function NavMenu({
     setIsLoggingOut(true);
     Axios.delete(`${apiUrl}logout`)
       .then((response) => {
-        console.log(response)
         localStorage.removeItem("token");
         userAuthProps.setUser(null);
         userAuthProps.setAuthenticated(false);
